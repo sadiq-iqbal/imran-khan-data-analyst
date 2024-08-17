f189c560eb4b037669c69ae43ffc029bbe709de1
sadiq iqbal <muhammadsadiq133570@gmail.com>
2024-08-17 07:42:18 +0500

    some small bug fixes

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'nunito': ['Nunito', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
